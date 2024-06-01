b6209b531824d5c823657d150da32b246b2c47e5
Daniela Guardia <138531935+DaniJGP@users.noreply.github.com>
2024-05-31 23:09:39 -0400

    Improve minus button code

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const spanCantidad = document.querySelector("#cantidad");
 const spanTotal = document.querySelector("#valor-total");
 const spanPrecio = document.querySelector("#precio-inicial");
 
-// Mostrar valores iniciales
+// Muestra valores iniciales
 
 spanPrecio.innerHTML = precio;
 spanTotal.innerHTML = total;
@@ -21,18 +21,16 @@ botonMas.addEventListener("click", () => {
   // Aumenta cantidad en 1 y recalcula total
   cantidad++;
   total = precio * cantidad;
-  // Actualiza elementos en la página
   spanCantidad.innerHTML = cantidad;
   spanTotal.innerHTML = total;
 });
 
 botonMenos.addEventListener("click", () => {
-  // Reduce cantidad en 1 y recalcula total
   if (cantidad > 0) {
+    // Reduce cantidad en 1 y recalcula total
     cantidad--;
+    total = precio * cantidad;
+    spanCantidad.innerHTML = cantidad;
+    spanTotal.innerHTML = total;
   }
-  total = precio * cantidad;
-  // Actualiza elementos en la página
-  spanCantidad.innerHTML = cantidad;
-  spanTotal.innerHTML = total;
 });
